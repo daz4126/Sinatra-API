56e3c4e56adf025456ffccccca0c4ea2aab81f47
DAZ <daz4126@gmail.com>
2013-08-30 17:33:55 +0100

    changed some filenames and added a readme file

--- test.rb
+++ test.rb
@@ -1,24 +1,42 @@
 ENV['RACK_ENV'] = 'test'
-
+require 'minitest/autorun'
+require 'rack/test'
 require_relative 'numb3rs.rb'
 
-require 'test/unit'
-require 'rack/test'
+include Rack::Test::Methods
 
-class FactorsTest < Test::Unit::TestCase
-  include Rack::Test::Methods
+def app
+  Sinatra::Application
+end
+  
+describe "Factorizor" do
 
-  def app
-    Sinatra::Application
+  it "should return the factors of 6" do
+    assert_equal [1,2,3,6], 6.factors
   end
   
-  def test_factors_method
-    assert_equal [[1,6],[2,3]], 6.factors
+  it "should say that 2 is prime" do
+    assert 2.prime?
   end
   
-  def test_get_factors
+  it "should say that 10 is not prime" do
+    refute 10.prime?
+  end
+
+  it "should return the factors of 6 as json" do
     get '/factors/6'
-    assert last_response.ok?
     assert_equal 6.factors.to_json, last_response.body
   end
+  
+  it "should return json" do
+    get '/info/6'
+    assert_equal last_response.headers['Content-Type'], 'application/json;charset=utf-8'
+  end 
+ 
+  it "should return info about 6 as json" do
+    get '/info/6'
+    six_info = { factors: 6.factors, odd: false, even: true, prime: false }
+    assert_equal six_info.to_json, last_response.body
+  end
+
 end
